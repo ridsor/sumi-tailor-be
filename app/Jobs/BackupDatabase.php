e9726b5b92e887e135e7b45fe8ac169ad6178713
ridsor <ryansyukur.it@gmail.com>
2024-06-23 22:01:41 +0900

    create a cronjob feat to delete old order and backup database

---
+++ app/Jobs/BackupDatabase.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\File;
+
+class BackupDatabase implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        $database = env('DB_DATABASE');
+        $username = env('DB_USERNAME');
+        $password = env('DB_PASSWORD');
+
+        $backupPath = 'storage/app/backups';
+
+        File::ensureDirectoryExists($backupPath);
+        $backupFilePath = $backupPath . '/' . date('Y-m-d_H-i-s') . '_backup.sql';
+        $command = "mysqldump -u ".$username." --password=".$password." $database > $backupFilePath";
+        exec($command);
+    }
+}
