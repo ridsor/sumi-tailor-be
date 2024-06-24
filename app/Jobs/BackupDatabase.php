443a9af26b51bba15686760e430751f1f0a0ee0b
ridsor <97663231+ridsor@users.noreply.github.com>
2024-06-24 22:18:20 +0900

    make feat backup order image

--- app/Jobs/BackupDatabase.php
+++ app/Jobs/BackupDatabase.php
@@ -10,6 +10,9 @@ use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\DB;
+use ZipArchive;
+use File as LFile;
 
 class BackupDatabase implements ShouldQueue
 {
@@ -36,15 +39,42 @@ class BackupDatabase implements ShouldQueue
         $username = env('DB_USERNAME');
         $password = env('DB_PASSWORD');
 
-        $backupPath = 'storage/app/backups';
+        $backupPath = storage_path('app\backups');
 
-        File::ensureDirectoryExists($backupPath);
-        $backupFilePath = $backupPath . '/';
+        File::ensureDirectoryExists($backupPath."/sql");
+        File::ensureDirectoryExists($backupPath."/image");
+        $backupFilePath = $backupPath . "/sql/";
         $nameFile = date('Y-m-d_H-i-s') . '_backup.sql';
         $backupFilePath .= $nameFile;
         $command = "mysqldump -u ".$username." --password=".$password." $database > $backupFilePath";
         exec($command);
 
-        Storage::disk('google')->put($nameFile, Storage::disk('local')->get('backups/'.$nameFile));
+        $orders = DB::table('order_history')->select('image')->whereRaw("updated_at >= NOW() - INTERVAL 1 WEEK")->get();
+        $nameFileZip = date('Y-m-d_H-i-s').'_order_images.zip';
+        
+        $destinationFileZip = $backupPath."\\image\\".$nameFileZip;
+        $sourceFolder = public_path('order-images');
+        $zip = new ZipArchive;
+        
+        if ($zip->open($destinationFileZip, ZipArchive::CREATE) === TRUE)
+        {
+            $files = LFile::files($sourceFolder);
+            
+            foreach ($files as $key => $value) {
+                $relativeNameInZipFile = basename($value);
+
+                foreach($orders as $order) {
+                    if($order->image == $relativeNameInZipFile) {
+                        $zip->addFile($value, $relativeNameInZipFile);
+                    }
+                }
+            }
+             
+            $zip->close();
+        }
+
+        Storage::disk('google')->put($nameFile, Storage::disk('local')->get('backups/sql/'.$nameFile));
+        Storage::disk('google')->put($nameFileZip, Storage::disk('local')->get('backups/image/'.$nameFileZip));
+        Storage::disk('local')->delete('backups/image/'.$nameFileZip);
     }
 }
--- config/cors.php
+++ config/cors.php
@@ -18,7 +18,7 @@ return [
     'paths' => ['/*'],
 
     'allowed_methods' => ['*'],
-
+  
     'allowed_origins' => ['http://localhost:3000'],
 
     'allowed_origins_patterns' => [],
